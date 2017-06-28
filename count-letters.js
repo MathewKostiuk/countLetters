991dda38f54eba60b282917466e4cb6f0d2c0915
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-06-28 20:48:02 +0000

    final product

--- count-letters.js
+++ count-letters.js
@@ -1,9 +1,8 @@
 var countObject = {};
 var re = /[a-zA-z]/g;
 var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-countLetters("oog a booga hallelujah");
-var sentence = "hello";
 
+/* Beta
 function countLetters(sentence, currentChar) {
   var count = 0;
   for (var i = 0; i < sentence.length; i++) {
@@ -17,5 +16,22 @@ for (var i = 0; i < characters.length; i++) {
   var currentChar = characters[i];
   countObject[currentChar] = countLetters(sentence, currentChar);
 }
-
 console.log(countObject);
+*/
+
+// Final Product
+function countLetters(sentence) {
+  var letters = sentence.toLowerCase().match(re);
+  console.log(letters);
+  for (var i = 0; i < characters.length; i++) {
+    var count = 0;
+    for (var x = 0; x < letters.length; x++) {
+      if (letters[x] === characters[i]) {
+        count++
+      }
+      countObject[characters[i]] = count;
+    }
+  }
+  console.log(countObject);
+}
+countLetters("HALLELUJAH IT FINALLY IS A FUNCTIONAL FUNCTION");
